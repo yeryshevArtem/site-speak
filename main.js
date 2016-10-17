edc1c395597632966d753ef699715f2c7c25e5f6
user <truebatmandc@gmail.com>
2016-10-17 10:44:17 +0300

    Added listener for form element. We get around all fields of form and check them to valid

---
+++ main.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', callback, false);
+'use strict';
+function callback () {
+  
+  preloadAudio([sounds.username_required,
+    sounds.password_required,
+  ]);
+
+  let form = document.getElementById('myForm');
+  form.addEventListener('submit', function (event) {
+    let resultsOfValidation = [];
+    for (let field of entries(fieldsForValidation)) {
+      switch (field.getAttribute('data-speak')) {
+        case 'username':
+          resultsOfValidation.push(rules.username.validate(field));
+          break;
+        case 'password':
+          resultsOfValidation.push(rules.password.validate(field));
+          break;
+      }
+    }
+    function isValid (element, index, array) {
+      return element === true;
+    }
+    if (resultsOfValidation.every(isValid)) {
+      alert('ok!');
+      // send post request to server
+    } else {
+      event.preventDefault();
+      for (let [indexOfInvalid, valueOfInvalid] of resultsOfValidation.entries()) {
+        if (!valueOfInvalid) {
+          preloadAudio.list[indexOfInvalid].play();
+          break;
+        }
+      }
+    }
+  }, false);
+};
