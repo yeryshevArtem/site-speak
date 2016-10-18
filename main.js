5a6cb3abc73459af52b091507a4518dd1c372778
user <truebatmandc@gmail.com>
2016-10-18 18:08:47 +0300

    Put all in single 'speechForm' function, that get form selector, preload audio elements and checking to valid all fields for that form.

--- main.js
+++ main.js
@@ -1,50 +1,8 @@
 'use strict';
-import { entries } from './modules/entries.js';
-import { fieldsForValidation } from './modules/fieldsForValidation.js';
-import { rules } from './modules/rules.js';
-import { sounds } from './modules/sounds.js';
-import { preloadAudio } from './modules/preload.js';
+import { speechForm } from './modules/speechForm.js';
 
 window.addEventListener('load', callback, false);
 
 function callback () {
-  let preload = new Promise (function (resolve, reject) {
-    resolve(preloadAudio([sounds.username_required,
-      sounds.password_required,
-    ]));
-  });
-
-  preload.then(function () {
-    let form = document.getElementById('myForm');
-    form.addEventListener('submit', function (event) {
-      let resultsOfValidation = [];
-      for (let field of entries(fieldsForValidation)) {
-        switch (field.getAttribute('data-speak')) {
-          case 'username':
-            resultsOfValidation.push(rules.username.validate(field));
-            break;
-          case 'password':
-            resultsOfValidation.push(rules.password.validate(field));
-            break;
-        }
-      }
-      function isValid (element, index, array) {
-        return element === true;
-      }
-      if (resultsOfValidation.every(isValid)) {
-        alert('ok!');
-        // send post request to server
-      } else {
-        event.preventDefault();
-        for (let [indexOfInvalid, valueOfInvalid] of resultsOfValidation.entries()) {
-          if (!valueOfInvalid) {
-            preloadAudio.list[indexOfInvalid].play();
-            break;
-          }
-        }
-      }
-    }, false);
-  }).catch(function (error) {
-    alert(error);
-  });
+  speechForm("#myForm");
 }
